8ae70f1f94101c8add4c69a36d95f44db3d3c2f6
Francisco1692qzd <franciscomine16432@gmail.com>
2025-05-12 22:21:11 -0300

    Update lightReplacer.lua

--- lightReplacer.lua
+++ lightReplacer.lua
@@ -1,1 +1,16 @@
 --// Lights
+function GetAllRooms()
+    local rooms = nil
+    for i, v in pairs(workspace.CurrentRooms:GetChildren()) do
+        rooms = v
+    end
+end
+
+function GetAddedRooms()
+    local addedRoom = nil
+    workspace.CurrentRooms.ChildAdded:Connect(function(child)
+        wait(0.5)
+        addedRoom = child
+    end)
+end
+
